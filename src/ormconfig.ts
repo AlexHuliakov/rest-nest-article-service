2f59a3e2243de159eda4f77e43638d4ca50b5157
AlexRazor <alexrazor1337@gmail.com>
2023-07-12 11:15:12 +0300

    Tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,11 +1,13 @@
-import { ConnectionOptions } from "typeorm";
+import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-    type: "postgres",
-    host: "localhost",
-    database: "nest-article",
-    username: "admin",
-    password: "password",
-}
+  type: 'postgres',
+  host: 'localhost',
+  database: 'nest-article',
+  username: 'admin',
+  password: 'password',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
 
 export default config;
